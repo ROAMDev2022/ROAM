ea78839876ff3d17a545ba0774327af96975fbc5
ROAMDev2022 <118581770+ROAMDev2022@users.noreply.github.com>
2022-11-19 10:39:00 +0800

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,13 @@
+user_permission={
+  [4]	= "READ_INSTALL_SESSIONS" ;
+  [1]	= "ACCESS_NETWORK_STATE" ;
+  [2]	= "ACCESS_WIFI_STATE" ;
+  [3]	= "INTERNET" ;
+  } ;
+template="bottom"
+appcode="23002000"
+appname="ROAM"
+appver="23.0.0"
+packagename="com.addplus.PBA.newyear"
+
+--require "import" import"loadlayout" import"loadmenu" import"loadbitmap" 
